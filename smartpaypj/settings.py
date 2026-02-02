29f0c0746c630eba8bbe36374297b7e1b00bf792
jermaine001 <bjamine254@gmail.com>
2026-02-02 08:24:59 +0300

    backup

--- smartpaypj/settings.py
+++ smartpaypj/settings.py
@@ -118,8 +118,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfile')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-MEDIA_URLS ='/media/'
+MEDIA_URL ='/media/'
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
